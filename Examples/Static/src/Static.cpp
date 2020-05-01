0acf9a347b58e40baec6c428e8064e2d9a097a22
RafaPraga <rafael.olarte@hotmail.com>
2020-05-01 18:30:02 -0500

    Exercises were carried out.

--- Examples/Static/src/Static.cpp
+++ Examples/Static/src/Static.cpp
@@ -13,13 +13,39 @@
 
 using namespace std;
 
+/*Exercise 4: Try incrementing static int field with a postfix increment operator.*/
+int NewsVideo::countInt = 0;
 
 
 int main() {
 
-	NewsVideo video1;
+	cout << NewsVideo::ORIGINAL_SOURCE_Str << endl; /*Exercise 1: Define and output a public static constant*/
+	cout << "----------------------------" << endl;
+	NewsVideo::viewConstantList();	/*Exercise 3: Output a static field with a static member.*/
 
+	cout << "----------------------------" << endl;
 
+	
+
+	NewsVideo video1{150, 30, 10};
+
+	cout << "----------------------------" << endl;
+	cout << video1.getLengthLng() << endl;  /*Exercise 2: Define and output a non-static field.*/
+	cout << video1.getWidthInt() << endl;
+	cout << video1.getHeightLng() << endl;
+	cout << video1.getOrderInt() << endl;
+
+	cout << "----------------------------" << endl;
+
+	NewsVideo video2{ 1000, 300, 100 };
+
+	cout << "----------------------------" << endl;
+	cout << video2.getLengthLng() << endl;
+	cout << video2.getWidthInt() << endl;
+	cout << video2.getHeightLng() << endl;
+	cout << video2.getOrderInt() << endl;
+
+	cout << "----------------------------" << endl; 
 
 	return 0;
 }
--- my-CPlusPlusBeginner-MultiProject/myHeader.h
+++ my-CPlusPlusBeginner-MultiProject/myHeader.h
@@ -1,1 +1,63 @@
 #pragma once
+
+
+
+#include<iostream>
+
+class NewsVideo
+{
+public:
+	/*Exercise 1: Define and output a public static constant*/
+	static constexpr const char* ORIGINAL_SOURCE_Str = "http://source.co"; /*You have to use constexpr in order to initialize nonintegers*/
+	static const int MAX_LENGTH_Int = 500;
+
+	/*Exercise 4: Try incrementing static int field with a postfix increment operator.*/
+	static int countInt;
+
+	/*Exercise 3: Output a static field with a static member.*/
+	static void viewConstantList()
+	{
+		std::cout << "Constants in this class:" << std::endl
+			<< "ORIGINAL_SOURCE_Str = " << ORIGINAL_SOURCE_Str << std::endl
+			<< "MAX_LENGTH_Int = " << MAX_LENGTH_Int << std::endl;
+	};
+
+private:
+	int orderInt; 
+	long lengthLng;  /*Exercise 2: Define and output a non-static field.*/
+	int widthInt;
+	int heightInt;
+
+public:
+	NewsVideo(long lengthInputLng, int widthInputInt, int heightInputInt) :
+		lengthLng{ lengthInputLng },
+		widthInt{ widthInputInt },
+		heightInt{ heightInputInt }
+	{
+		orderInt = countInt++; /*Exercise 4: Try incrementing static int field with a postfix increment operator.*/
+		std::cout << "NewsVideo objected was constructed." << std::endl;
+
+	};
+
+	/*Exercise 2: Define and output a non-static field.*/
+	long getLengthLng()
+	{
+		return lengthLng;
+	};
+
+	int getWidthInt()
+	{
+		return widthInt;
+	};
+
+	int getHeightLng()
+	{
+		return heightInt;
+	};
+
+	int getOrderInt()
+	{
+		return orderInt;
+	};
+
+};
