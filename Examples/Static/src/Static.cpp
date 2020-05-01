c5f38467cd3dcddd567cfdf96d75abd7a1f520d4
RafaPraga <rafael.olarte@hotmail.com>
2020-05-01 15:19:10 -0500

    Project was cleaned up for exercise.

--- Examples/Static/src/Static.cpp
+++ Examples/Static/src/Static.cpp
@@ -6,47 +6,20 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+
 #include <iostream>
+#include "myHeader.h"
+
+
 using namespace std;
 
-// .h header file
-class Test {
-public:
-	// Initialisation of const must be done right here!
-	static int const MAX = 99;
 
-private:
-	int id;
-	static int count;
-
-public:
-	Test() {
-		id = count++;
-	}
-
-	int getId() {
-		return id;
-	}
-
-	static void showInfo() {
-		cout << count << endl;
-	}
-};
-
-// .cpp source
-int Test::count = 0;
 
 int main() {
 
-	cout << Test::MAX << endl;
+	NewsVideo video1;
 
-	Test test1;
-	cout << "Object 1 ID: " << test1.getId() << endl;
 
-	Test test2;
-	cout << "Object 2 ID: " << test2.getId() << endl;;
-
-	Test::showInfo();
 
 	return 0;
 }
